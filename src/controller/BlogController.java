7449378cf6efe75304fc942130515e04a352a16e
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-14 13:19:53 +0530

    last

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Blog;
 import model.User;
+import utility.CheckBlogPost;
 
 
 @WebServlet(urlPatterns= {"/blog"})
@@ -21,6 +22,7 @@ public class BlogController extends HttpServlet {
 
     public BlogController() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 
@@ -47,14 +49,19 @@ public class BlogController extends HttpServlet {
 		blog.setBlogTitle(title);
 		blog.setBlogDescription(description);
 		blog.setDate(postedOn);
-
-		if(checkblog!) {
+		
+	
+		
+		CheckBlogPost check =new CheckBlogPost();
+		boolean checkblog= check.checkBlog(blog);
+		if(checkblog!=false) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-		else{
+	
+else{
 			
 			request.setAttribute("error", "Your blog cannot be added as it contains offensive words, Please check your blog");
 
@@ -63,6 +70,7 @@ public class BlogController extends HttpServlet {
 			
 			
 		}
+		
 		
 	}
 
--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,43 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker  implements  OffensiveWordsInterface{
+	
+	String[] bad_words = {"ahole","bitch","cock","dick","erotic","fcuk","gay","heroin","injun","jerk","kill","lesbo","murder","niggle","orally","pawn","quicky","racy","sadist","teez","uterus","virgin","weed","xxx","yobbo","zibbi"};
+
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+
+		 String title=blog.getBlogTitle();
+		 for(int i=0;i<25;i++) {
+			 if(title.equalsIgnoreCase( bad_words[i])) {
+				 return true;
+				 
+			 }
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		String description=blog.getBlogDescription();
+		for(int i=0;i<bad_words.length;i++)
+			{
+			if(description.equalsIgnoreCase(bad_words[i]))
+				return true;
+		}
+	     return false;
+	}
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+	
+		if((checkBlogTitle(blog))||(checkBlogDescription(blog))) {
+			return false;
+		}
+		return true;
+	}
+
+}
